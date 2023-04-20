18d7974d3d411da2d16c2905198979b224ee957e
AmayAvasthi <amayavasthi12@gmail.com>
2023-04-20 19:15:21 +0530

    adding bootstrap

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
+const path = require ('path');
 
 const port = 3000;
 
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+
 app.get('/', (req, res) => res.send ("Welcome to my app built with mean"));
+
+
+app.get('/frontPage' ,(req, res) => {res.sendFile(path.join(__dirname, 'views/index.html'))});
 
 app.get('/firstPage' , function (req, res) {
     res.send("this is first page created by routing protocols using express and node")
