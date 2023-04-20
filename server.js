86ed440b485228a3a0e4065168f85289bb8e34fe
AmayAvasthi <amayavasthi12@gmail.com>
2023-04-20 19:05:52 +0530

    second commit

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send ("Welcome to my app built with mean"));
 
+app.get('/firstPage' , function (req, res) {
+    res.send("this is first page created by routing protocols using express and node")
+});
+
 app.listen(port, () => console.log("App listening on port"));
